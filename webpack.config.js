5c04f51e3e4772f15ce7992fe373970e76b84265
Emiliano Durán <emiliano.duran@binamics.com.ar>
2019-08-08 20:14:47 -0300

    serve dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, './src/app.js'),
@@ -6,10 +8,27 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack starter project',
+            template: path.resolve('./src/index.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+
+        })
+
+    ],
     module: {
         rules: [{
             test: /\.css$/,
-            use: ['style-loader', 'css-loader']
-        }]
-    }
+            use: [{
+                    loader: MiniCssExtractPlugin.loader
+                },
+                'css-loader',
+            ],
+        }],
+    },
 }
