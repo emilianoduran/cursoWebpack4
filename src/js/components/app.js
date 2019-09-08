0286b31d62dc875e1910b94dfdecbe602036c296
Emiliano Durán <emiliano.duran@binamics.com.ar>
2019-09-08 02:46:41 -0300

    trabajando con react resultado

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data.json";
+import Loader from "./loader";
 
 function App() {
+  const [loaderList, setLoaderList] = useState([]);
+  function handleClick() {
+    setLoaderList(data.loaders);
+  }
   return (
-    <div class="wrapper">
+    <div>
       <h1> React en acción </h1>
       <p>Esto es un ejemplo de react con webpack</p>
+      <ul>
+        {loaderList.map(item => (
+          <Loader {...item} key={item.id} />
+        ))}
+      </ul>
+      <button onClick={handleClick}>
+        Mostrar lo aprendido hasta el momento
+      </button>
     </div>
   );
 }
--- src/js/louncher.js
+++ src/js/louncher.js
@@ -1,12 +1,13 @@
 import "../css/styles.css";
 import React from "react";
 import { render } from "react-dom";
+import data from "./components/data.json";
 import App from "./components/app";
 
 function louncher() {
   console.log("🚀 louncherr 🚀");
   // código react
-
+  console.log(data);
   // @ts-ignore
   render(<App />, document.querySelector(".container"));
 }
